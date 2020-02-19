45b671f0496ea7068f404e575a390a66e5020746
Guid88K <dkfl242324232423>
2020-02-19 19:12:31 +0200

    voice phon

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('english/phonetics');
 });
 Route::get('/dictionary', function () {
     return view('pages.dictionary');
