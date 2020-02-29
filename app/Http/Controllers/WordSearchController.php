4fa74e51e1386709d4cf681addb5d8551477c0f6
Guid88K <dkfl242324232423>
2020-02-29 17:32:08 +0200

    1

---
+++ app/Http/Controllers/WordSearchController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Dictionary;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class WordSearchController extends Controller
+{
+    public function search(Request $request)
+    {
+        $dictionary = DB::table('dictionaries')->where('word', 'like', '%' . $request->word . '%')->get();
+
+        if (!(count($dictionary) > 0)) {
+            $dictionary = DB::table('dictionaries')->where('translate_word', 'like', '%' . $request->word . '%')->get();
+
+        }
+
+        return view('dictionary.dictionary', ['dictionary' => $dictionary]);
+    }
+}
